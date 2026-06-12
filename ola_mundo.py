13d933a147face226809311315055418f2f8bc45
Guilherme Henrique <guilhermehsilveira.95@gmail.com>
2026-06-12 16:53:30 -0300

    feat: archive ola_mundo.py added to the project

---
+++ ola_mundo.py
@@ -0,0 +1,6 @@
+print("Olá, Mundo!")
+
+# Esse é um comentário em Python. Ele é ignorado pelo interpretador e serve para explicar o código. 
+print("Bem-vindo ao Python!")
+print("Com Python, você pode criar aplicativos, jogos, sites e muito mais!")
+print("Meu nome é Guilherme")
