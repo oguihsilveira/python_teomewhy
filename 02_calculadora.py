786f42ac09deaa3ef9b3fe535fddf78c0206d468
Guilherme Silveira <guilhermehsilveira.95@gmail.com>
2026-06-13 12:20:28 -0300

    feat: new exercises done

---
+++ 02_calculadora.py
@@ -0,0 +1,24 @@
+# %%
+
+# SOMA
+print("1 + 1 =", 1 + 1)
+
+# SUBTRAÇÃO
+print("10 - 5 =", 10 - 5)
+
+# MULTIPLICAÇÃO
+print ("10 x 10 =", 10 * 10)
+
+# DIVISÃO
+print("100 / 5 =", 100 / 5)
+
+# POTENCIAÇÃO
+print("2 ^ 8 =", 2 ** 8)
+
+# DIVISÃO INTEIRA
+print("100 // 5 =", 100 // 5)
+
+# RESTO DA DIVISÃO
+print("100 % 5 =", 100 % 5)
+
+# %%
