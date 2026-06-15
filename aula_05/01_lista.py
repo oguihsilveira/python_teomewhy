0875cdd32ad824b23cf953f14d09277237a676e6
Guilherme Silveira <guilhermehsilveira.95@gmail.com>
2026-06-15 08:41:45 -0300

    feat: updates before starting next class on monday

---
+++ aula_05/01_lista.py
@@ -0,0 +1,17 @@
+# Criando uma lista de idades
+idades = [3-8] [1]
+print(idades) [9]
+
+# Verificando o tipo da variável
+print(type(idades)) [10]
+
+# Lista com dados variados (nome, sobrenome, idade, tem filhos, casado, renda)
+tel = ["Téo", "Calvo", 32, True, "casado", 2342.98] [11], [2], [12]
+# Acessando o primeiro elemento ("Téo")
+print(tel) [13]
+
+# Acessando o terceiro elemento (idade: 32)
+print(tel[1]) [13]
+
+# Acessando o último elemento usando índice negativo
+print(tel[-1]) [14]
