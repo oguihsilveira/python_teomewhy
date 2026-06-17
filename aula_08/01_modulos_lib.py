de32530c9e41286f023522a16c371d8f7c53899e
Guilherme Henrique <guilhermehsilveira.95@gmail.com>
2026-06-17 14:42:50 -0300

    feta: class 8 exercises done

---
+++ aula_08/01_modulos_lib.py
@@ -0,0 +1,10 @@
+import math # Importa o módulo math para acessar suas funções e constantes
+from math import sqrt, pow, e, pi # Importa as funções sqrt e pow, e as constantes e e pi do módulo math para acessar diretamente essas funções e constantes sem precisar usar o prefixo math.
+
+math.sqrt(16) # Chama a função sqrt do módulo math para calcular a raiz quadrada de 16 e imprime o resultado
+
+math.pow(2, 3) # Chama a função pow do módulo math para calcular 2 elevado a 3 e imprime o resultado
+
+math.e # Imprime o valor da constante e do módulo math
+
+math.pi # Imprime o valor da constante pi do módulo math
---
+++ aula_08/02_aleatorio.py
@@ -0,0 +1,7 @@
+import random
+
+random.randint(1, 10)  # Gera um número inteiro aleatório entre 1 e 10 (inclusive)
+
+x = ["Guilherme", "Maria", "João", "Ana"]
+
+random.choice(x)  # Escolhe um elemento aleatório da lista x
